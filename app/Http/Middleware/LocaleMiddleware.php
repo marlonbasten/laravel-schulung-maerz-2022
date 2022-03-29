7c52765abe935cfd5990f4897b6e508218fc1bb4
Marlon Basten <mbasten@byte5.de>
2022-03-29 10:40:04 +0200

    optional middleware parameters

--- app/Http/Middleware/LocaleMiddleware.php
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -14,9 +14,11 @@ class LocaleMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, string $locale = null)
     {
-        $locale = session('locale', 'en');
+        if(!$locale) {
+            $locale = session('locale', 'en');
+        }
 
         app()->setLocale($locale);
 
